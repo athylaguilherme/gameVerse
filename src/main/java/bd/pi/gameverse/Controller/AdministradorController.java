b5fb48b089bc56bb6c7d3b8f7a5e1037cecdd165
Jose <jose.silva@brisabr.com.br>
2024-11-17 18:35:44 -0300

    Criado update de admin

--- src/main/java/bd/pi/gameverse/Controller/AdministradorController.java
+++ src/main/java/bd/pi/gameverse/Controller/AdministradorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,10 @@ public class AdministradorController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable Long id) {
         return ResponseEntity.ok().body(AdministradorsService.deletarAdministrador(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateAdmin(@PathVariable long id, @RequestBody Administrador admin) {
+        return ResponseEntity.ok().body(AdministradorsService.alterarAdministrador(admin));
     }
 }
